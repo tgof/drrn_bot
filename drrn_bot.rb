19de6e6815da92b675d8d05dc5ed7142b0647e87
Alexander Alekseev <rnarron@gmail.com>
2017-07-20 10:59:38 +0300

    Initial commit.

---
+++ drrn_bot.rb
@@ -0,0 +1,152 @@
+require 'telegram/bot'
+require 'net/http'
+
+token = File.read('data/token.txt', encoding: 'UTF-8')
+
+def help_msg
+%Q{Я умею:
+	* /roll 3d6 - брось дайсы!
+	* /for_the_emperor - Мотивирующая фраза от вашего лорда-комиссара.
+	* /qr_it - Сделать qr-код.
+	* Нихуя
+А еще я сплю большую часть времени.
+}
+end
+
+def roll(text)
+	res = text.scan(/\d+/).map(&:to_i)
+	p res
+	if res.size == 2
+		rolls = []
+		return 'Куда тебе столько, ебанутый?' if res[0] > 1000
+		return 'Нуль себе дерни, пес' if res[1] == 0
+		res[0].times do |n|
+			rolls << rand(res[1]) + 1
+		end
+		sum = rolls.inject(0) do |res, x|
+			res + x
+		end
+		"Бросок #{res[0]}d#{res[1]}: #{sum} (#{rolls.join(', ')})."
+	else
+		return 'Че-то ты криво рольнул.'
+	end
+end
+
+def qr_it(message, bot)
+	query = message.text.sub(/\/qr_it\s+/,'')
+	url = qr_url(query)
+	bot.api.send_photo(chat_id: message.chat.id, photo: url, reply_to_message_id: message.message_id)
+	nil
+end
+
+def qr_url(query)
+	"http://chart.apis.google.com/chart?chs=300x300&cht=qr&choe=UTF-8&chl=#{query}"
+end
+
+def wh40kquote
+	@quotes ||= File.read('data/warhammer_quotes.txt', encoding: 'UTF-8').split("\n")
+	@quotes.sample
+end
+
+def handle_message(message, bot)
+	begin
+		p text = message.text && message.text.gsub('@drrn_bot','').strip
+		case text
+		when '/start'
+			"Ну привет, #{message.from.first_name}"
+		when '/stop'
+			"Покеда, #{message.from.first_name}"
+		when '/help'
+			help_msg
+		when /\/qr_it\s.+/
+			qr_it(message, bot)
+		when '/vzhuh'
+			'Сам себе вжухай.'
+		when /Now you.+thinking with portals!/, '/portals'
+			# 'Шас жахнет!'
+			bot.api.send_sticker(chat_id: message.chat.id, sticker: 'CAADAgADEgAD3Q_4SCfsQNkInMIsAg')
+			# nil
+		when '/for_the_emperor', 'За Императора!'
+			wh40kquote
+		when '/heresy'
+			kb = [
+				Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Да', callback_data: "#{message.chat.id}~ересь"),
+				Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Нет', callback_data: "#{message.chat.id}~не ересь")
+			]
+			markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
+			bot.api.send_message(chat_id: message.chat.id, text: 'Вы подозреваете ересь?', reply_markup: markup)
+			nil
+		when '/roll'
+			'Че кидать-то будем?'
+		when /\/roll\s\d+d\d+/
+			roll(text)
+		end
+	rescue => e then
+		e.to_s
+	end
+end
+
+def handle_inline(message, bot)
+	p query = message.query
+	results = [
+		[1, 'Пожать плечами', "#{query} ¯\\_(ツ)_/¯"],
+		[2, 'За Императора!', wh40kquote]
+	]
+	results << [3, '...чертов гук!', goddamn_guk(query)] if query.size > 0
+	results.map do |arr|
+		Telegram::Bot::Types::InlineQueryResultArticle.new(
+			id: arr[0],
+			title: arr[1],
+			input_message_content: Telegram::Bot::Types::InputTextMessageContent.new(message_text: arr[2])
+		)
+	end
+end
+
+def goddamn_guk(str)
+	@guk_str ||= " чёртов гук! Эти сукины сыны научились прятаться даже там! \
+Я позвал ребят и мы начали палить что есть силы по этому чёртовому полю, \
+мне даже прострелили каску, Джонни, это был просто ад, а не перестрелка! \
+Нашего сержанта ранили, мы оттащили его в окоп и перевязали там же.\n— Ребята, \
+передайте моей матери… — начал сержант Лейнисон.\n— Ты сам ей всё передашь, \
+чёртов камикадзе!\nИ тогда мы вызвали наших ребят, наших славных соколов, \
+которые сбросили на этих гуков напалм. Ты бы видел это, парень! Когда я \
+приходил на это поле, оно было то зелёное, то золотое, а теперь оно ещё долго \
+будет золотым и лишь потом почернеет, поглотив своей чернотой гуков. Я люблю \
+запах напалма поутру. Весь холм был им пропитан. Это был запах… победы!\n\
+Когда-нибудь эта война закончится."
+	str + @guk_str
+end
+
+def handle_callback(message, bot)
+	p data = message.data
+	case data.split(?~).last
+	when 'ересь' then 'Возможно, ересь.'
+	when 'не ересь' then 'Хреновый из вас инквизитор.'
+	end
+end
+
+Telegram::Bot::Client.run(token) do |bot|
+	bot.listen do |message|
+		case message
+		when Telegram::Bot::Types::InlineQuery
+			results = handle_inline(message, bot)
+			begin
+				bot.api.answer_inline_query(inline_query_id: message.id, results: results, cache_time: 1)
+			rescue => e then puts(e)
+			end
+		when Telegram::Bot::Types::Message
+			res = handle_message(message, bot)
+			begin
+				bot.api.send_message(chat_id: message.chat.id, text: res, reply_to_message_id: message.message_id) if res.is_a? String
+			rescue => e then puts(e)
+			end
+		when Telegram::Bot::Types::CallbackQuery
+			# Here you can handle your callbacks from inline buttons
+			res = handle_callback(message, bot)
+			begin
+				bot.api.send_message(chat_id: message.data.split(?~).first, text: res) if res.is_a? String
+			rescue => e then puts(e)
+			end
+		end
+	end
+end
