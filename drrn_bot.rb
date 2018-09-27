6d792681fa1844286f3376738c97d37c54d3f9d0
Rnarron <rnarron@gmail.com>
2018-09-27 16:20:00 +0300

    Запихаем бота и сообщение в собак.
    Чтоб по параметрам не таскать.

--- drrn_bot.rb
+++ drrn_bot.rb
@@ -100,32 +100,32 @@ def fur_sausage
  \/"""```]
 end
 
-def send_markdown_message(reply, message, bot)
-	bot.api.send_message(chat_id: message.chat.id, text: reply, reply_to_message_id: message.message_id, parse_mode: 'Markdown')
+def send_markdown_message(text)
+	@bot.api.send_message(chat_id: @message.chat.id, text: text, reply_to_message_id: @message.message_id, parse_mode: 'Markdown')
 	nil
 end
 
-def handle_message(message, bot)
-	puts "#{message.from.first_name}: #{message.text}"
-	case message.text
+def handle_message
+	puts "#{message.from.first_name}: #{@message.text}"
+	case @message.text
 	when /^\/start(@drrn_bot)?$/
-		"Ну привет, #{message.from.first_name}"
+		"Ну привет, #{@message.from.first_name}"
 	when /^\/stop(@drrn_bot)?$/
-		"Покеда, #{message.from.first_name}"
+		"Покеда, #{@message.from.first_name}"
 	when /^\/help(@drrn_bot)?$/
 		help_msg
 	when /^\/qr_it(@drrn_bot)?\s+.+/
-		qr_it(message, bot)
+		qr_it(@message, @bot)
 	when /^\/(vzhuh|magic)(@drrn_bot)?(\s+.*|$)/
 		return 'Сам себе вжухай!' if rand > 0.9
-		query = message.text.sub(/\/(vzhuh|magic)(@drrn_bot)?\s*/, '')
-		res = vzhuh_str(query)
-		send_markdown_message(res, message, bot) if res.is_a? String
+		query = @message.text.sub(/\/(vzhuh|magic)(@drrn_bot)?\s*/, '')
+		res = vzhuh_str query
+		send_markdown_message(res) if res.is_a? String
 	when /^\/shrug(@drrn_bot)?(\s+.*|$)/
-		query = message.text.sub(/\/shrug(@drrn_bot)?\s*/, '')
+		query = @message.text.sub(/\/shrug(@drrn_bot)?\s*/, '')
 		"#{query}¯\\_(ツ)_/¯"
 	when /^\/(cppref|tableflip)(@drrn_bot)?(\s+.*|$)/, /блэт/i, /жеваный крот/i
-		query = message.text.sub(/\/(cppref|tableflip)(@drrn_bot)?\s*/, '')
+		query = @message.text.sub(/\/(cppref|tableflip)(@drrn_bot)?\s*/, '')
 		"#{query} #{tableflip_str}"
 	when /Now you.+thinking with portals!/, /^\/portals(@drrn_bot)?/
 		'Шас жахнет!'
@@ -135,53 +135,53 @@ def handle_message(message, bot)
 		wh40kquote
 	when /^\/heresy(@drrn_bot)?$/
 		kb = [
-			Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Да', callback_data: "#{message.chat.id}~ересь"),
-			Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Нет', callback_data: "#{message.chat.id}~не ересь")
+			Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Да', callback_data: "#{@message.chat.id}~ересь"),
+			Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Нет', callback_data: "#{@message.chat.id}~не ересь")
 		]
 		markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
-		bot.api.send_message(chat_id: message.chat.id, text: 'Вы подозреваете ересь?', reply_markup: markup)
+		@bot.api.send_message(chat_id: @message.chat.id, text: 'Вы подозреваете ересь?', reply_markup: markup)
 		nil
 	when /^\/update_and_restart(@drrn_bot)?(\s+.*|$)/
-		return 'Пошел нахуй.' unless admin_ids.include?(message.from.id)
+		return 'Пошел нахуй.' unless admin_ids.include?(@message.from.id)
 		delta = Time.now - start_time
 		if delta < 60 # если перегружались меньше минуты назад
 			return "Сейчас мы тут: #{%x{git show --oneline -s}}\nДо следующего возможного перезапуска #{(60 - delta).to_i} секунд."
 		end
-		query = message.text.sub(/\/update_and_restart(@drrn_bot)?\s*/, '')
+		query = @message.text.sub(/\/update_and_restart(@drrn_bot)?\s*/, '')
 		if query.size > 0
-			bot.api.send_message(chat_id: message.chat.id, text: "Пробуем чекаутить #{query}")
+			@bot.api.send_message(chat_id: @message.chat.id, text: "Пробуем чекаутить #{query}")
 			res = %x{git checkout #{query}}
-			bot.api.send_message(chat_id: message.chat.id, text: res)
+			@bot.api.send_message(chat_id: @message.chat.id, text: res)
 		end
-		bot.api.send_message(chat_id: message.chat.id, text: 'Ок, перегружаюсь.')
+		@bot.api.send_message(chat_id: @message.chat.id, text: 'Ок, перегружаюсь.')
 		sleep 5
 		abort # просто пристрелить себя, демон сам все сделает
 	when /^\/roll(@drrn_bot)?\s*$/
 		'Че кидать-то будем?'
 	when /^\/roll(@drrn_bot)?\s+\d+d\d+(\s*[\>\<\=CcСс]\d+)?/
-		query = message.text.sub(/\/roll(@drrn_bot)?\s*/, '')
+		query = @message.text.sub(/\/roll(@drrn_bot)?\s*/, '')
 		roll(query)
 	when /^\/taft(_?test)?(@drrn_bot)?(\s+\d+\s+\d+)?/
-		query = message.text.sub(/\/taft(_?test)?(@drrn_bot)?\s*/, '')
+		query = @message.text.sub(/\/taft(_?test)?(@drrn_bot)?\s*/, '')
 		params = query.scan(/\d+/)
 		width, height = params
 		width  ||= rand(1500) + 100
 		height ||= rand(1500) + 100
 		url = "https://tafttest.com/#{width}x#{height}.png"
-		bot.api.send_photo(chat_id: message.chat.id, photo: url, reply_to_message_id: message.message_id)
+		@bot.api.send_photo(chat_id: @message.chat.id, photo: url, reply_to_message_id: @message.message_id)
 		nil
 	when /^\/lenny_?face(@drrn_bot)?(\s+.*|$)/
-		query = message.text.sub(/\/lenny_?face(@drrn_bot)?\s*/, '')
+		query = @message.text.sub(/\/lenny_?face(@drrn_bot)?\s*/, '')
 		"#{query} ( ͡° ͜ʖ ͡°)"
 	when /шерстяная колбаса/i
-		send_markdown_message(fur_sausage, message, bot)
+		send_markdown_message fur_sausage
 	end
 rescue => e then
 	e.to_s
 end
 
-def handle_inline(message, bot)
-	p query = message.query
+def handle_inline
+	p query = @message.query
 	i = 1
 	results = [
 		[(i += 1), 'Пожать плечами', {message_text: "#{query} ¯\\_(ツ)_/¯"}],
@@ -217,8 +217,8 @@ def goddamn_guk(str)
 	str + @guk_str
 end
 
-def handle_callback(message, bot)
-	p data = message.data
+def handle_callback
+	p data = @message.data
 	case data.split('~').last
 	when 'ересь' then 'Возможно, ересь.'
 	when 'не ересь' then 'Хреновый из вас инквизитор.'
@@ -226,25 +226,27 @@ def handle_callback(message, bot)
 end
 
 Telegram::Bot::Client.run(token) do |bot|
+	@bot = bot
 	bot.listen do |message|
-		case message
+		@message = message
+		case @message
 		when Telegram::Bot::Types::InlineQuery
-			results = handle_inline(message, bot)
+			results = handle_inline
 			begin
-				bot.api.answer_inline_query(inline_query_id: message.id, results: results, cache_time: 1)
+				@bot.api.answer_inline_query(inline_query_id: @message.id, results: results, cache_time: 1)
 			rescue => e then puts(e)
 			end
 		when Telegram::Bot::Types::Message
-			res = handle_message(message, bot)
+			res = handle_message
 			begin
-				bot.api.send_message(chat_id: message.chat.id, text: res, reply_to_message_id: message.message_id) if res.is_a? String
+				@bot.api.send_message(chat_id: @message.chat.id, text: res, reply_to_message_id: @message.message_id) if res.is_a? String
 			rescue => e then puts(e)
 			end
 		when Telegram::Bot::Types::CallbackQuery
 			# Here you can handle your callbacks from inline buttons
-			res = handle_callback(message, bot)
+			res = handle_callback
 			begin
-				bot.api.edit_message_text(chat_id: message.data.split(?~).first, message_id: message.message.message_id, text: res) if res.is_a? String
+				@bot.api.edit_message_text(chat_id: @message.data.split(?~).first, message_id: @message.message.message_id, text: res) if res.is_a? String
 			rescue => e then puts(e)
 			end
 		end
