6b5bb5ee8900a4736c4b2a9c7460a8ea53ed3219
Rnarron Dragonash <rnarron@gmail.com>
2022-01-01 02:58:48 +0300

    Новогод

--- drrn_bot.rb
+++ drrn_bot.rb
@@ -540,7 +540,7 @@ def handle_message
   when /с новым годом/i
     now = Time.now
     if (now.day == 31 && now.month == 12 && now.hour >= 15) || (now.day == 1 && now.month == 1)
-      "С новым годом, чертила #{@message.from.first_name}!"
+      "С новым, блин, годом, #{@message.from.first_name}!"
     else
       nil
     end
